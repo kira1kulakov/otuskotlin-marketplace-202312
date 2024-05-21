7e2f5ff37e5262079a31cd758ec5adb3c3cd2e6f
Kirill Kulakov <kkulakov@it-one.ru>
2024-05-21 19:27:40 +0300

    add module 'social-moodnow-app-spring';

---
+++ social-moodnow-be/social-moodnow-app-spring/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependencies)
+    alias(libs.plugins.spring.kotlin)
+    alias(libs.plugins.kotlinx.serialization)
+    id("build-jvm")
+}
+
+dependencies {
+    implementation(libs.spring.actuator)
+    implementation(libs.spring.webflux)
+    implementation(libs.spring.webflux.ui)
+    implementation(libs.jackson.kotlin)
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
+
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.reactor)
+    implementation(libs.coroutines.reactive)
+    implementation(libs.kotlinx.serialization.core)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(project(":social-moodnow-common"))
+    implementation(project(":social-moodnow-app-common"))
+    implementation("ru.otus.otuskotlin.social.moodnow.libs:social-moodnow-lib-logging-logback")
+
+    implementation(project(":social-moodnow-api-v1-jackson"))
+    implementation(project(":social-moodnow-api-v1-mappers"))
+
+    implementation(project(":social-moodnow-api-v2-kmp"))
+
+    implementation(project(":social-moodnow-biz"))
+
+    // tests
+    testImplementation(kotlin("test-junit5"))
+    testImplementation(libs.spring.test)
+    testImplementation(libs.mockito.kotlin)
+}
+
+tasks {
+    withType<ProcessResources> {
+        val files = listOf("spec-v1", "spec-v2").map {
+            rootProject.ext[it]
+        }
+        from(files) {
+            into("/static")
+            filter {
+                // Устанавливаем версию в сваггере
+                it.replace("\${VERSION_APP}", project.version.toString())
+            }
+
+        }
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ social-moodnow-be/social-moodnow-app-spring/src/test/kotlin/ApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.otus.otuskotlin.social.moodnow.app.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
